2676b9ba386c0b5d58adbcaf5be265cab9c1a98c
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-05-01 16:20:00 +0600

    Added multiple state slices to the store | Used those state slices in different components throughout the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./Components/Counter";
+import Header from "./Components/Header";
+import Auth from "./Components/Auth";
+import UserProfile from "./Components/UserProfile";
 
 function App() {
-  return <Counter />;
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <>
+      <Header />
+      {isAuth ? <UserProfile /> : <Auth />}
+      <Counter />;
+    </>
+  );
 }
 
 export default App;
